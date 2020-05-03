3dbf78014886446d7f80b0e0d919466f78d18567
eiji <eijishimizu9@gmail.com>
2020-05-03 17:37:16 +0900

    不要なハンドラクラスを削除

--- src/RequestHandlerFactory/QueueRequestHandlerFactory.cpp
+++ src/RequestHandlerFactory/QueueRequestHandlerFactory.cpp
@@ -1,12 +1,7 @@
 #include "LSQ.h"
 #include "ServerApplication/Subsystems/LSQSubsystem.h"
 #include "RequestHandlerFactory/QueueRequestHandlerFactory.h"
-#include "RequestHandler/BulkRequestHandler.h"
-#include "RequestHandler/LaterOnRequestHandler.h"
-#include "RequestHandler/LaterRequestHandler.h"
 #include "RequestHandler/PopRequestHandler.h"
-#include "RequestHandler/PushOnRequestHandler.h"
-#include "RequestHandler/PushRawRequestHandler.h"
 #include "RequestHandler/PushRequestHandler.h"
 #include "RequestHandler/SizeRequestHandler.h"
 #include "RequestHandler/CommandRequestHandler.h"
@@ -20,22 +15,12 @@ namespace LSQ
 {
 // URL
 const std::string LSQ = "lsq";
-const std::string BULK = "/" + LSQ + "/bulk";
-const std::string LATER_ON = "/" + LSQ + "/laterOn";
-const std::string LATER = "/" + LSQ + "/later";
 const std::string POP = "/" + LSQ + "/pop";
-const std::string PUSH_ON = "/" + LSQ + "/pushOn";
-const std::string PUSH_RAW = "/" + LSQ + "/pushRaw";
 const std::string PUSH = "/" + LSQ + "/push";
 const std::string SIZE = "/" + LSQ + "/size";
 
 // ハンドラーファクトリー関数
-HTTPRequestHandler *BulkRequestHandlerFactory(const HTTPServerRequest &) { return new BulkRequestHandler; }
-HTTPRequestHandler *LaterOnRequestHandlerFactory(const HTTPServerRequest &) { return new LaterOnRequestHandler; }
-HTTPRequestHandler *LaterRequestHandlerFactory(const HTTPServerRequest &) { return new LaterRequestHandler; }
 HTTPRequestHandler *PopRequestHandlerFactory(const HTTPServerRequest &) { return new PopRequestHandler; }
-HTTPRequestHandler *PushOnRequestHandlerFactory(const HTTPServerRequest &) { return new PushOnRequestHandler; }
-HTTPRequestHandler *PushRawRequestHandlerFactory(const HTTPServerRequest &) { return new PushRawRequestHandler; }
 HTTPRequestHandler *PushRequestHandlerFactory(const HTTPServerRequest &) { return new PushRequestHandler; }
 HTTPRequestHandler *SizeRequestHandlerFactory(const HTTPServerRequest &) { return new SizeRequestHandler; }
 
@@ -44,12 +29,7 @@ HTTPRequestHandler *CommandRequestHandlerFactory(const HTTPServerRequest &) { re
 
 QueueRequestHandlerFactory::QueueRequestHandlerFactory()
 {
-    factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_POST, BULK), BulkRequestHandlerFactory));
-    factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_POST, LATER_ON), LaterOnRequestHandlerFactory));
-    factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_POST, LATER), LaterRequestHandlerFactory));
     factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_POST, POP), PopRequestHandlerFactory));
-    factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_POST, PUSH_ON), PushOnRequestHandlerFactory));
-    factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_POST, PUSH_RAW), PushRawRequestHandlerFactory));
     factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_POST, PUSH), PushRequestHandlerFactory));
     factorys_.insert(std::make_pair(std::make_pair(HTTPRequest::HTTP_GET, SIZE), SizeRequestHandlerFactory));
 }
